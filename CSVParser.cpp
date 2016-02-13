a5a9b9c2de9f57410f8284606e7f3c3ce7fcc416
mitchlane <mitchlane44@gmail.com>
2016-02-12 18:33:36 -0800

    starting with minicsv

--- CSVParser.cpp
+++ CSVParser.cpp
@@ -1,43 +1,12 @@
 #include <stdio.h>
-#include <iostream>
 #include <stdlib.h>
 #include <errno.h>
 #include <string>
 #include <vector>
-#include "csv.h"
+#include "minicsv.h"
+#include <iostream>
 using namespace std;
-using namespace io;
-
-/**
- * CSV Reader functions
-template<
-  unsigned column_count,
-  class trim_policy = trim_chars<' ', '\t'>, 
-  class quote_policy = no_quote_escape<','>,
-  class overflow_policy = throw_on_overflow,
-  class comment_policy = no_comment
->
-class CSVReader{
-public:
-  // Constructors
-  // same as for LineReader
-
-  // Parsing Header
-  void read_header(ignore_column ignore_policy, some_string_type col_name1, some_string_type col_name2, ...);
-  void set_header(some_string_type col_name1, some_string_type col_name2, ...);
-  bool has_column(some_string_type col_name)const;
-
-  // Read
-  bool read_row(ColType1&col1, ColType2&col2, ...);
-
-  // File Location 
-  void set_file_line(unsigned);
-  unsigned get_file_line(unsigned)const;
-  void set_file_name(some_string_type file_name);
-  const char*get_truncated_file_name()const;
-};
-
-*/
+using namespace csv;
 
 typedef struct airport
 {
@@ -63,26 +32,24 @@ int main(int argc, char *argv[])
   airport ap = {vector<int>(10000), vector<string>(10000),
                 vector<string>(10000), vector<string>(10000),
                 vector<double>(10000), vector<double>(10000)};
-  string bogus;
-
-  CSVReader<12> in("airports.csv");
-  in.set_header("aid", "name", "city", "country", "code", "icao", "lat", "lon", "alt", "timeZone", "dst","tz");
-
-  // Forcing me to read in every column...
- // in.read_header(ignore_extra_column, "aid", "name", "city", "country","code", "icao","lat", "lon", "alt", "timeZone", "dst","tz");
 
   int count = 0;
-  printf("here\n");
-  while(in.read_row(ap.aid[count], ap.name[count], ap.city[count], bogus, ap.code[count], bogus, ap.lat[count], ap.lon[count], bogus, bogus, bogus, bogus))
+  csv::ifstream is("airports.dat", std::ios_base::in);
+  is.set_delimiter(',');
+  int tempID;
+  string tempName, tempCity, tempCode, tempBlank;
+  double tempLat, tempLong, tempBlankDouble;
+  
+  if(is.to_open())
   {
-    cout << ap.name[count] << endl;
-    ++count;
+    while(is.read_line())
+    {
+      is >> tempID >> tempName >> tempCity >> tempBlank >> tempCode
+         >> tempBlank >> tempLat >> tempLon >> tempBlankDouble >> tempBlankDouble;
+      printf("%d\n", tempID);
+    }
   }
   
-  for(int i : ap.aid)
-  {
-    cout << "aid = " << i << endl;
-  } 
   
   return 0;
 }
